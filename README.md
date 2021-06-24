e78eb1efd136be77f280139e3095005a8c37396b
qiandayu17 <qian.d@husky.neu.edu>
2021-06-23 23:38:12 -0400

    47

--- README.md
+++ README.md
@@ -9,3 +9,4 @@ LeetCode
 |90|[Subsets II](https://leetcode.com/problems/subsets-ii/) | [Java](./Solutions/90.java)|Medium|
 |332|[Reconstruct Itinerary](https://leetcode.com/problems/reconstruct-itinerary/) | [Java](./Solutions/332.java)|Medium|
 |216|[Combination Sum III](https://leetcode.com/problems/combination-sum-iii/submissions/) | [Java](./Solutions/216.java)|Medium|
+|47|[Permutations II](https://leetcode.com/problems/permutations-ii/) | [Java](./Solutions/47.java)|Medium|
---
+++ Solutions/47.java
@@ -0,0 +1,34 @@
+47. Permutations II
+
+// dfs with backtracking. for each position, we fill in the first element of
+// all the duplicates. because after that, if we fill in another element that
+// is the same as the first element, the permutation would be duplicate.
+
+class Solution {
+    List<List<Integer>> ans = new ArrayList<>();
+
+    public List<List<Integer>> permuteUnique(int[] nums) {
+        Arrays.sort(nums);
+        List<Integer> ls = new ArrayList<>();
+        boolean[] visited = new boolean[nums.length];
+        dfs(nums, ls, visited);
+        return ans;
+    }
+    private void dfs(int[] nums, List<Integer> ls, boolean[] visited) {
+        if (ls.size() == nums.length) {
+            ans.add(new ArrayList<>(ls));
+            return;
+        }
+        
+        for (int i = 0; i < nums.length; i++) {
+            if (i > 0 && nums[i] == nums[i-1] && !visited[i-1]) continue;
+            if (!visited[i]) {
+                visited[i] = true;
+                ls.add(nums[i]);
+                dfs(nums, ls, visited);
+                ls.remove(ls.size()-1);
+                visited[i] = false;
+            }
+        }
+    }
+}
